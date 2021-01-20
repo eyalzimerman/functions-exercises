235820c531cee4096f62a61feaecfdd542b28719
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-20 14:47:39 +0200

    solution to q6 (bonus)

--- Exercises.js
+++ Exercises.js
@@ -55,8 +55,16 @@ function getFirstNotRepeating(str) {
 
 //Question 6 (Bonus)
 function isPrefectNumber(num) {
-  // your code here
-  return "I'm not interested in the bonus question :(";
+  let sum = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      sum = sum + i;
+    }
+  }
+  if (sum === num) {
+    return true;
+  }
+  return false;
 }
 
 // *** Playground ***
